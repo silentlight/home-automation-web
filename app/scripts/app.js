ed6a223be0287b6b84ed914d8406c1477f7913f4
Bartosz Hejman <bartoszhejman@gmail.com>
2015-01-10 22:50:39 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name App
+ * @description
+ * # App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('HomeAutomationApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
