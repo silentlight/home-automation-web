e99e90834dad9f8fed1928658ff1283cc0390301
Bartosz Hejman <bartoszhejman@gmail.com>
2015-01-17 01:02:33 +0100

    Update inputs view for led

--- src/app/components/inputs/inputsController.js
+++ src/app/components/inputs/inputsController.js
@@ -1,18 +1,23 @@
 inputsModule.controller('inputsController', function ($scope, inputsService) {
 
+    $scope.currentLedStatus = -1;
+
     $scope.ledOn = function(){
         inputsService.led.setStatus(1).success(function(data){
+            $scope.currentLedStatus = 1;
             alert("Done!");
         });
     };
 
     $scope.ledOff = function(){
         inputsService.led.setStatus(0).success(function(data){
+            $scope.currentLedStatus = 0;
             alert("Done!");
         });
     };
 
     inputsService.led.getStatus().success(function(data){
-        alert('Led is currently ' + (data.data == '1' ? 'on' : 'off'))
+        $scope.currentLedStatus = data.data;
+        alert('Led is currently ' + (data.data == '1' ? 'on' : 'off'));
     });
 });
